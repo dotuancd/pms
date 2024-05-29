23ce2b49fe183206cb9edbd60dc02e8c96b1ba64
Thi Thao Phuong Ta <tathaophuong1905@gmail.com>
2024-05-29 11:01:33 +0700

    feat: reformat code

--- packages/ui/src/lib/molecules/CodeEditor.svelte
+++ packages/ui/src/lib/molecules/CodeEditor.svelte
@@ -1,128 +1,128 @@
 <script lang="ts">
-	import { html } from '@codemirror/lang-html';
-	import { json, jsonParseLinter } from '@codemirror/lang-json';
-	import { xml } from '@codemirror/lang-xml';
-	import { linter } from '@codemirror/lint';
-	import { StateEffect } from '@codemirror/state';
-	import { showPanel, type Panel } from '@codemirror/view';
-	import prettierPluginXml from "@prettier/plugin-xml";
-	import { EditorView, basicSetup } from 'codemirror';
-	import * as prettier from "prettier";
-	import prettierPluginBabel from "prettier/plugins/babel";
-	import prettierPluginEstree from "prettier/plugins/estree";
-	import prettierPluginHtml from "prettier/plugins/html";
-	import { onDestroy, onMount } from 'svelte';
+  import { html } from '@codemirror/lang-html';
+  import { json, jsonParseLinter } from '@codemirror/lang-json';
+  import { xml } from '@codemirror/lang-xml';
+  import { linter } from '@codemirror/lint';
+  import { StateEffect } from '@codemirror/state';
+  import { showPanel, type Panel } from '@codemirror/view';
+  import prettierPluginXml from "@prettier/plugin-xml";
+  import { EditorView, basicSetup } from 'codemirror';
+  import * as prettier from "prettier";
+  import prettierPluginBabel from "prettier/plugins/babel";
+  import prettierPluginEstree from "prettier/plugins/estree";
+  import prettierPluginHtml from "prettier/plugins/html";
+  import { onDestroy, onMount } from 'svelte';
 
-	export let language: string;
-	export let value: string | null;
-	let highlighter: HTMLElement;
-	let editor: EditorView;
+  export let language: string;
+  export let value: string | null;
+  let highlighter: HTMLElement;
+  let editor: EditorView;
 
-	const theme = EditorView.theme({
-		'.cm-gutters': {
-			backgroundColor: 'oklch(var(--b1))',
-			borderRightColor: 'oklch(var(--bc)/var(--tw-border-opacity))'
-		},
-		'.cm-panels-top': {
-			borderBottom: '1px solid oklch(var(--bc)/var(--tw-border-opacity)) !important'
-		},
-		'.cm-panels': {
-			backgroundColor: 'transparent !important'
-		}
-	});
+  const theme = EditorView.theme({
+    '.cm-gutters': {
+      backgroundColor: 'oklch(var(--b1))',
+      borderRightColor: 'oklch(var(--bc)/var(--tw-border-opacity))'
+    },
+    '.cm-panels-top': {
+      borderBottom: '1px solid oklch(var(--bc)/var(--tw-border-opacity)) !important'
+    },
+    '.cm-panels': {
+      backgroundColor: 'transparent !important'
+    }
+  });
 
-	function getLanguage(lang: string) {
-		switch (lang) {
-			case 'json':
-				return [json(), linter(jsonParseLinter())];
-			case 'xml':
-				return [xml()];
-			case 'html':
-				return [html()];
-			default:
-				return [];
-		}
-	}
+  function getLanguage(lang: string) {
+    switch (lang) {
+      case 'json':
+        return [json(), linter(jsonParseLinter())];
+      case 'xml':
+        return [xml()];
+      case 'html':
+        return [html()];
+      default:
+        return [];
+    }
+  }
 
-	function getParser(lang: string): prettier.Options {
-		switch (lang) {
-			case 'json':
-				return {parser: 'json', plugins: [prettierPluginBabel, prettierPluginEstree]}
-			case 'xml':
-			return {parser: 'babel', plugins: [prettierPluginBabel, prettierPluginEstree, prettierPluginXml]};
-			case 'html':
-				return {parser: 'html', plugins: [prettierPluginHtml]};
-			default:
-				return {};
-		}
-	}
+  function getParser(lang: string): prettier.Options {
+    switch (lang) {
+      case 'json':
+        return {parser: 'json', plugins: [prettierPluginBabel, prettierPluginEstree]}
+      case 'xml':
+        return {parser: 'babel', plugins: [prettierPluginBabel, prettierPluginEstree, prettierPluginXml]};
+      case 'html':
+        return {parser: 'html', plugins: [prettierPluginHtml]};
+      default:
+        return {};
+    }
+  }
 
-	function beautifierPanel(view: EditorView): Panel {
-		let dom = document.createElement('div');
-		dom.className = 'grid justify-items-end bg-base-100';
-		let button = document.createElement('button');
-		button.className = 'btn btn-ghost btn-xs my-1 mr-1 text-gray-500';
-		button.textContent = 'Beautify';
-		button.addEventListener('click', async (event) => {
-			event.preventDefault();
-			const text = await prettier.format(
-				editor.state.doc.toString(), getParser(language))
-			editor.dispatch({
-				changes: {from: 0, to: editor.state.doc.length, insert: text}
-			})
-		});
-		dom.appendChild(button);
-		return { top: true, dom };
-	}
+  function beautifierPanel(view: EditorView): Panel {
+    let dom = document.createElement('div');
+    dom.className = 'grid justify-items-end bg-base-100';
+    let button = document.createElement('button');
+    button.className = 'btn btn-ghost btn-xs my-1 mr-1 text-gray-500';
+    button.textContent = 'Beautify';
+    button.addEventListener('click', async (event) => {
+      event.preventDefault();
+      const text = await prettier.format(
+        editor.state.doc.toString(), getParser(language))
+      editor.dispatch({
+        changes: {from: 0, to: editor.state.doc.length, insert: text}
+      })
+    });
+    dom.appendChild(button);
+    return { top: true, dom };
+  }
 
-	const beautifier = showPanel.of(beautifierPanel);
+  const beautifier = showPanel.of(beautifierPanel);
 
-	function recreateEditor(lang: string) {
-		if (!highlighter) {
-			return;
-		}
+  function recreateEditor(lang: string) {
+    if (!highlighter) {
+      return;
+    }
 
-		const extensions = [basicSetup, theme, ...getLanguage(lang), beautifier];
+    const extensions = [basicSetup, theme, ...getLanguage(lang), beautifier];
 
-		if (editor) {
-			editor.dispatch({
-				effects: StateEffect.reconfigure.of(extensions)
-			});
-		} else {
-			editor = new EditorView({
-				doc: value || '',
-				extensions,
-				parent: highlighter
-			});
-		}
-	}
+    if (editor) {
+      editor.dispatch({
+        effects: StateEffect.reconfigure.of(extensions)
+      });
+    } else {
+      editor = new EditorView({
+        doc: value || '',
+        extensions,
+        parent: highlighter
+      });
+    }
+  }
 
-	$: {
-		recreateEditor(language);
-	}
+  $: {
+    recreateEditor(language);
+  }
 
-	onMount(() => {
-		recreateEditor(language);
+  onMount(() => {
+    recreateEditor(language);
 
-		highlighter.addEventListener('keyup', () => {
-			value = editor.state.doc.toString();
-		});
-	});
+    highlighter.addEventListener('keyup', () => {
+      value = editor.state.doc.toString();
+    });
+  });
 
-	onDestroy(() => {
-		editor?.destroy();
-	});
+  onDestroy(() => {
+    editor?.destroy();
+  });
 </script>
 
 <div bind:this={highlighter} class="min-h-100 w-full code-editor" />
 
 <style>
-	.code-editor {
-		--tw-border-opacity: 0.2;
-		border: solid 1px oklch(var(--bc) / var(--tw-border-opacity));
-	}
+  .code-editor {
+    --tw-border-opacity: 0.2;
+    border: solid 1px oklch(var(--bc) / var(--tw-border-opacity));
+  }
 
-	.code-editor::selection {
-		background-color: red;
-	}
+  .code-editor::selection {
+    background-color: red;
+  }
 </style>
--- packages/ui/src/lib/organisms/StaticStrategy.svelte
+++ packages/ui/src/lib/organisms/StaticStrategy.svelte
@@ -1,208 +1,208 @@
 <script lang="ts">
-	import type { StaticStrategy } from '$lib/Strategy';
-	import DeleteIcon from '$lib/atoms/DeleteIcon.svelte';
-	import CodeEditor from '$lib/molecules/CodeEditor.svelte';
+  import type { StaticStrategy } from '$lib/Strategy';
+  import DeleteIcon from '$lib/atoms/DeleteIcon.svelte';
+  import CodeEditor from '$lib/molecules/CodeEditor.svelte';
 
-	let contentTypes = new Map([
-		['text/plain', 'text', ],
-		['application/json', 'json', ],
-		['text/html', 'html',],
-		['application/xml', 'xml']
-	]);
+  let contentTypes = new Map([
+    ['text/plain', 'text', ],
+    ['application/json', 'json', ],
+    ['text/html', 'html',],
+    ['application/xml', 'xml']
+  ]);
     
-	type Header = { key: string; value: string };
-	export let value: StaticStrategy;
-	export let idPrefix = '';
-	let selected = 'headers';
-	let bodyType = value.options.body ? 'raw' : 'none';
-	let contentType = getContentTypeFromHeaders(value.options.headers);
-	let languageForEditor = getLanguageByContentType(getContentTypeFromHeaders(value.options.headers));
-	let headersForEditor: Header[] = [];
+  type Header = { key: string; value: string };
+  export let value: StaticStrategy;
+  export let idPrefix = '';
+  let selected = 'headers';
+  let bodyType = value.options.body ? 'raw' : 'none';
+  let contentType = getContentTypeFromHeaders(value.options.headers);
+  let languageForEditor = getLanguageByContentType(getContentTypeFromHeaders(value.options.headers));
+  let headersForEditor: Header[] = [];
 
-	headersForEditor = value.options.headers.map((header) => {
-		return {
-			key: header.key,
-			value: header.value
-		};
-	});
+  headersForEditor = value.options.headers.map((header) => {
+    return {
+      key: header.key,
+      value: header.value
+    };
+  });
 
-	headersForEditor.push({ key: '', value: '' });
+  headersForEditor.push({ key: '', value: '' });
 
-	function getContentTypeFromHeaders(headers: Header[]) {
-		return headers.find(h => h.key.toLowerCase() == "content-type")?.value || "text/plain"
-	}
+  function getContentTypeFromHeaders(headers: Header[]) {
+    return headers.find(h => h.key.toLowerCase() == "content-type")?.value || "text/plain"
+  }
 
-	function getLanguageByContentType(contentType: string) {
-		// @TODO: support the charset option.
-		return contentTypes.get(contentType) || "text"
-	}
+  function getLanguageByContentType(contentType: string) {
+    // @TODO: support the charset option.
+    return contentTypes.get(contentType) || "text"
+  }
 
-	function addNewIfLastElement(index: number) {
-		if (index == headersForEditor.length - 1) {
-			headersForEditor.push({ key: '', value: '' });
-		}
-	}
+  function addNewIfLastElement(index: number) {
+    if (index == headersForEditor.length - 1) {
+      headersForEditor.push({ key: '', value: '' });
+    }
+  }
 
-	function deleteHeader(index: number) {
-		headersForEditor = headersForEditor.filter((_, i) => i !== index);
-	}
+  function deleteHeader(index: number) {
+    headersForEditor = headersForEditor.filter((_, i) => i !== index);
+  }
 
-	function addContentType(contentType: string) {
-		headersForEditor = [
-			{ key: 'Content-Type', value: contentType },
-			...headersForEditor.filter((header) => header.key.toLowerCase() !== 'content-type')
-		];
-	}
+  function addContentType(contentType: string) {
+    headersForEditor = [
+      { key: 'Content-Type', value: contentType },
+      ...headersForEditor.filter((header) => header.key.toLowerCase() !== 'content-type')
+    ];
+  }
 
-	$: {
-		if (bodyType === 'none') {
-			value.options.body = null;
-		}
+  $: {
+    if (bodyType === 'none') {
+      value.options.body = null;
+    }
 
-		value.options.headers = headersForEditor.filter((header) => header.key !== '');
-		languageForEditor = getLanguageByContentType(contentType)
-	}
+    value.options.headers = headersForEditor.filter((header) => header.key !== '');
+    languageForEditor = getLanguageByContentType(contentType)
+  }
 </script>
 
 <div>
-	<div class="flex justify-between items-center">
-		<div role="tablist" class="tabs tabs-boxed flex flex-row gap-4">
-			<button
-				role="tab"
-				type="button"
-				on:click={() => (selected = 'headers')}
-				class="tab"
-				class:tab-active={selected == 'headers'}
-			>
-				Headers <span class="ml-1 badge badge-success badge-outline"
-					>{headersForEditor.length - 1}</span
-				>
-			</button>
-			<button
-				role="tab"
-				type="button"
-				on:click={() => (selected = 'body')}
-				class="tab"
-				class:tab-active={selected == 'body'}>Body</button
-			>
-		</div>
-		<div class="flex flex-row items-center">
-			<label for="status">Status</label>
-			<input
-				class="input input-sm w-20 font-bold rounded-none"
-				class:text-success={value.options.status < 300}
-				class:text-warning={value.options.status >= 300 && value.options.status < 400}
-				class:text-error={value.options.status >= 400}
-				min="100"
-				max="599"
-				type="number"
-				id="status"
-				bind:value={value.options.status}
-			/>
-		</div>
-	</div>
+  <div class="flex justify-between items-center">
+    <div role="tablist" class="tabs tabs-boxed flex flex-row gap-4">
+      <button
+        role="tab"
+        type="button"
+        on:click={() => (selected = 'headers')}
+        class="tab"
+        class:tab-active={selected == 'headers'}
+      >
+        Headers <span class="ml-1 badge badge-success badge-outline"
+          >{headersForEditor.length - 1}</span
+        >
+      </button>
+      <button
+        role="tab"
+        type="button"
+        on:click={() => (selected = 'body')}
+        class="tab"
+        class:tab-active={selected == 'body'}>Body</button
+      >
+    </div>
+    <div class="flex flex-row items-center">
+      <label for="status">Status</label>
+      <input
+        class="input input-sm w-20 font-bold rounded-none"
+        class:text-success={value.options.status < 300}
+        class:text-warning={value.options.status >= 300 && value.options.status < 400}
+        class:text-error={value.options.status >= 400}
+        min="100"
+        max="599"
+        type="number"
+        id="status"
+        bind:value={value.options.status}
+      />
+    </div>
+  </div>
 </div>
 
 <div>
-	{#if selected === 'headers'}
-		<div class="py-2">Headers</div>
-		<table class="table">
-			<thead>
-				<tr>
-					<th>Key</th>
-					<th>Value</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each headersForEditor as header, index}
-					<tr>
-						<td class="p-1">
-							<input
-								class="input input-sm w-full rounded-none"
-								type="text"
-								id="{idPrefix}_{index}_hk"
-								on:input={() => addNewIfLastElement(index)}
-								bind:value={header.key}
-								placeholder="Key"
-							/>
-						</td>
-						<td>
-							<input
-								class="input input-sm w-full rounded-none"
-								type="text"
-								id="{idPrefix}_{index}_hv"
-								on:input={() => addNewIfLastElement(index)}
-								bind:value={header.value}
-								placeholder="Value"
-							/>
-						</td>
-						<td class="w-10 text-end">
-							{#if index < headersForEditor.length - 1}
-								<button
-									type="button"
-									class="p-1 rounded hover:bg-slate-100"
-									on:click={() => deleteHeader(index)}
-								>
-									<DeleteIcon class="text-sm" size={24} />
-								</button>
-							{/if}
-						</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
-	{/if}
-	{#if selected === 'body'}
-		<div class="my-2 flex flex-row content-between gap-2">
-			<div class="form-control">
-				<label class="label cursor-pointer">
-					<input
-						type="radio"
-						bind:group={bodyType}
-						name="{idPrefix}_body"
-						value="none"
-						class="radio"
-					/>
-					<span class="label-text ml-2">none</span>
-				</label>
-			</div>
-			<div class="form-control">
-				<label class="label cursor-pointer">
-					<input
-						type="radio"
-						bind:group={bodyType}
-						name="{idPrefix}_body"
-						on:click={() => addContentType('text')}
-						value="raw"
-						class="radio"
-					/>
-					<span class="label-text ml-2">raw</span>
-				</label>
-			</div>
-			{#if bodyType === 'raw'}
-				<div>
-					<select
-						class="select select-bordered select-sm rounded-box"
-						bind:value={contentType}
-						on:change={(e) => addContentType(e.target?.value)}
-						name="{idPrefix}_body_raw"
-						id="{idPrefix}_body-raw_preset"
-					>
-						<option value="text/plain">Text</option>
-						<option value="application/json">JSON</option>
-						<option value="text/html">HTML</option>
-						<option value="text/xml">XML</option>
-					</select>
-				</div>
-			{/if}
-		</div>
-		{#if bodyType === 'none'}
-			<div class="bg-base-200 rounded-box py-4 text-gray-800 text-center text-sm">
-				This response does not have a body
-			</div>
-		{/if}
-		{#if bodyType === 'raw'}
-				<CodeEditor bind:language={languageForEditor} bind:value={value.options.body}/>
-		{/if}
-	{/if}
+  {#if selected === 'headers'}
+    <div class="py-2">Headers</div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Key</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each headersForEditor as header, index}
+          <tr>
+            <td class="p-1">
+              <input
+                class="input input-sm w-full rounded-none"
+                type="text"
+                id="{idPrefix}_{index}_hk"
+                on:input={() => addNewIfLastElement(index)}
+                bind:value={header.key}
+                placeholder="Key"
+              />
+            </td>
+            <td>
+              <input
+                class="input input-sm w-full rounded-none"
+                type="text"
+                id="{idPrefix}_{index}_hv"
+                on:input={() => addNewIfLastElement(index)}
+                bind:value={header.value}
+                placeholder="Value"
+              />
+            </td>
+            <td class="w-10 text-end">
+              {#if index < headersForEditor.length - 1}
+                <button
+                  type="button"
+                  class="p-1 rounded hover:bg-slate-100"
+                  on:click={() => deleteHeader(index)}
+                >
+                  <DeleteIcon class="text-sm" size={24} />
+                </button>
+              {/if}
+            </td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  {/if}
+  {#if selected === 'body'}
+    <div class="my-2 flex flex-row content-between gap-2">
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <input
+            type="radio"
+            bind:group={bodyType}
+            name="{idPrefix}_body"
+            value="none"
+            class="radio"
+          />
+          <span class="label-text ml-2">none</span>
+        </label>
+      </div>
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <input
+            type="radio"
+            bind:group={bodyType}
+            name="{idPrefix}_body"
+            on:click={() => addContentType('text')}
+            value="raw"
+            class="radio"
+          />
+          <span class="label-text ml-2">raw</span>
+        </label>
+      </div>
+      {#if bodyType === 'raw'}
+        <div>
+          <select
+            class="select select-bordered select-sm rounded-box"
+            bind:value={contentType}
+            on:change={(e) => addContentType(e.target?.value)}
+            name="{idPrefix}_body_raw"
+            id="{idPrefix}_body-raw_preset"
+          >
+            <option value="text/plain">Text</option>
+            <option value="application/json">JSON</option>
+            <option value="text/html">HTML</option>
+            <option value="text/xml">XML</option>
+          </select>
+        </div>
+      {/if}
+    </div>
+    {#if bodyType === 'none'}
+      <div class="bg-base-200 rounded-box py-4 text-gray-800 text-center text-sm">
+        This response does not have a body
+      </div>
+    {/if}
+    {#if bodyType === 'raw'}
+        <CodeEditor bind:language={languageForEditor} bind:value={value.options.body}/>
+    {/if}
+  {/if}
 </div>
